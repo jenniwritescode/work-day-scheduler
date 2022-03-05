9124ce566cfc261348e7afa6d40395b4766dbea2
Jenni Hartman <jenni.hartman@icloud.com>
2022-03-05 12:43:37 -0600

    add function to populate calendar items from local storage on document load using ready method

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,3 +27,63 @@ $(".saveBtn").on("click", function(event) {
     // Check to see if calendar item is saved in local storage
     console.log(calItem);
 });
+
+// Function to populate calendar items if they are in local storage using ready method
+$(document).ready(function () {
+    if (localStorage["saveNine"] !== null && localStorage["saveNine"] !== undefined) {
+      var calItemNine = $("<p>" + localStorage["saveNine"] + "</p>");
+      $("#textNine").append(calItemNine[0].innerText);
+    } 
+    else {
+      ("");
+    }
+    if (localStorage["saveTen"] !== null && localStorage["saveTen"] !== undefined) {
+        var calItemTen = $("<p>" + localStorage["saveTen"] + "</p>");
+        $("#textTen").append(calItemTen[0].innerText);
+      } 
+      else {
+        ("");
+      }
+      if (localStorage["saveEleven"] !== null && localStorage["saveEleven"] !== undefined) {
+        var calItemEleven = $("<p>" + localStorage["saveEleven"] + "</p>");
+        $("#textEleven").append(calItemEleven[0].innerText);
+      } 
+      else {
+        ("");
+      }
+      if (localStorage["saveTwelve"] !== null && localStorage["saveTwelve"] !== undefined) {
+        var calItemTwelve = $("<p>" + localStorage["saveTwelve"] + "</p>");
+        $("#textTwelve").append(calItemTwelve[0].innerText);
+      } 
+      else {
+        ("");
+      }
+      if (localStorage["saveOne"] !== null && localStorage["saveOne"] !== undefined) {
+        var calItemOne = $("<p>" + localStorage["saveOne"] + "</p>");
+        $("#textOne").append(calItemOne[0].innerText);
+      } 
+      else {
+        ("");
+      }
+      if (localStorage["saveTwo"] !== null && localStorage["saveTwo"] !== undefined) {
+        var calItemTwo = $("<p>" + localStorage["saveTwo"] + "</p>");
+        $("#textTwo").append(calItemTwo[0].innerText);
+      } 
+      else {
+        ("");
+      }
+      if (localStorage["saveThree"] !== null && localStorage["saveThree"] !== undefined) {
+        var calItemThree = $("<p>" + localStorage["saveThree"] + "</p>");
+        $("#textThree").append(calItemThree[0].innerText);
+      } 
+      else {
+        ("");
+      }
+      if (localStorage["saveFour"] !== null && localStorage["saveFour"] !== undefined) {
+        var calItemFour = $("<p>" + localStorage["saveFour"] + "</p>");
+        $("#textFour").append(calItemFour[0].innerText);
+      } 
+      else {
+        ("");
+      }
+});

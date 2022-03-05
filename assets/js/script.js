14bc49f8dfb5319b0bc129fa861329dd148e6a32
Jenni Hartman <jenni.hartman@icloud.com>
2022-03-05 12:17:33 -0600

    add function to save calendar  items to local storage when save button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,6 @@ var timeBlock = $(".hour");
 var timeNow = parseInt(moment().format("H"));
 
 // Decide if current time is past, present, or future in order to apply colors to time blocks
-
 $(timeBlock).each(function (i, hour) {
     var hourBlock = parseInt($(this).attr("id"));
     if (hourBlock === timeNow) {
@@ -19,4 +18,12 @@ $(timeBlock).each(function (i, hour) {
     else if (hourBlock < timeNow) {
         $(this).next().addClass("past");
     }
+});
+
+// Save calendar item to local storage when save button is clicked
+$(".saveBtn").on("click", function(event) {
+    var calItem = event.target.parentElement.previousElementSibling.children[0].value;
+    localStorage.setItem(event.target.attributes[0].value, calItem);
+    // Check to see if calendar item is saved in local storage
+    console.log(calItem);
 });

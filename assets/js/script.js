26aec2d1a402191d529bfd0f4af4ae9cf0c32620
Jenni Hartman <jenni.hartman@icloud.com>
2022-03-05 12:11:03 -0600

    add function to check to see if current time is past, present, or future to update color blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,18 @@ $("#currentDay").text(currentDate);
 // Set variables for time block section, use Moment for current time
 var timeBlock = $(".hour");
 var timeNow = parseInt(moment().format("H"));
+
+// Decide if current time is past, present, or future in order to apply colors to time blocks
+
+$(timeBlock).each(function (i, hour) {
+    var hourBlock = parseInt($(this).attr("id"));
+    if (hourBlock === timeNow) {
+        $(this).next().addClass("present");
+    }
+    else if (hourBlock > timeNow) {
+        $(this).next().addClass("future");
+    }
+    else if (hourBlock < timeNow) {
+        $(this).next().addClass("past");
+    }
+});
